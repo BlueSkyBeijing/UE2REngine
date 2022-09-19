a793d09c0578eae5561e9fdb599671bd7cefc691
chenjunlong <chenjunlong@kingsoft.com>
2022-09-19 09:13:05 +0800

    add sample map

---
+++ Source/UE2REngine/UE2REngineGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE2REngineGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE2RENGINE_API AUE2REngineGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
